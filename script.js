3bb10a71f780508dd6da9a5a414d51ff45bd9da7
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-11-19 14:40:42 +0530

    upgraded delete, edit and checkbox functionality

--- script.js
+++ script.js
@@ -11,25 +11,41 @@ document.addEventListener('DOMContentLoaded', () => {
         if (taskText) {
             const listItem = document.createElement('li');
             listItem.innerHTML = `
+                <input type="checkbox" class="mark-complete">
                 <span class="task-text">${taskText}</span>
                 <div>
-                    <input type="checkbox" class="mark-complete">
-                    <button class="delete-task">Delete</button>
+                    <i class="fas fa-edit edit-task"></i>
+                    <i class="fas fa-trash-alt delete-task"></i>
                 </div>
             `;
             taskList.appendChild(listItem);
             inputField.value = '';
             addTaskListeners(listItem);
+            alert('Task added!');
         }
     }
 
     function addTaskListeners(taskItem) {
         const deleteButton = taskItem.querySelector('.delete-task');
+        const editButton = taskItem.querySelector('.edit-task');
         const checkBox = taskItem.querySelector('.mark-complete');
+        const taskText = taskItem.querySelector('.task-text');
         
-        deleteButton.addEventListener('click', () => taskItem.remove());
+        deleteButton.addEventListener('click', () => {
+            if (confirm("Are you sure you want to delete this task?")) {
+                taskItem.remove();
+            }
+        });
+        editButton.addEventListener('click', () => editTask(taskItem, taskText));
         checkBox.addEventListener('change', () => {
             taskItem.classList.toggle('task-completed', checkBox.checked);
         });
+    }
+
+    function editTask(taskItem, taskTextElement) {
+        const newText = prompt("Edit your task:", taskTextElement.textContent);
+        if (newText !== null) {
+            taskTextElement.textContent = newText;
+        }
     }
 });
